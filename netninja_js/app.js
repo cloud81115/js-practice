50e93ecf4b90afc09213c5e1f313ec8883a9f763
ltbliu <cloud81115@hotmail.com.tw>
2018-08-12 14:28:25 +0800

    finished tutorial

--- netninja_js/app.js
+++ netninja_js/app.js
@@ -90,5 +90,41 @@ li.appendChild(deleteBtn);
 list.appendChild(li);
 
 })
-
-
+// hide books
+const hideBox = document.querySelector('#hide');
+hideBox.addEventListener('change',function(e){
+	if(hideBox.checked){
+		list.style.display="none";
+	}else{
+		list.style.display="initial";
+	}
+});
+// filter books
+const seatchBar = document.forms['search-books'].querySelector('input');
+seatchBar.addEventListener('keyup',function(e){
+	const term = e.target.value.toLowerCase();
+	const books = list.getElementsByTagName('li');
+	Array.from(books).forEach(function(book){
+		const title = book.firstElementChild.textContent;
+		if(title.toLowerCase().indexOf(term)!=-1){
+			book.style.display = 'block';
+		}else{
+			book.style.display = 'none';
+		}
+	})
+})
+// tabbed content
+// const tabs = document.querySelector('.tabs');
+// const panels = document.querySelectorAll('.panel');
+// tabs.addEventListener('click',function(e){
+// 	if(e.target.tagName=="LI"){
+// 		const targetPanel = document.querySelector(e.target.dataset.target);
+// 		panels.forEach(function(panel){
+// 			if(panel == targetPanel){
+// 				panel.classList.add('active');
+// 			}else{
+// 				panel.classList.remove('active');
+// 			}
+// 		})
+// 	}
+// })
